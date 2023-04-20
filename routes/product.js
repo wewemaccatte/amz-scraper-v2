7effd428935383f5da86b589d429298e29d44b60
Daniele <danielericcelli@me.com>
2023-04-20 21:36:54 +0200

    v3

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const router = express.Router();
 
-const app = express();
-
-app.get('/api/product/:asin', async (req, res) => {
+router.get('/:asin', async (req, res) => {
     const asin = req.params.asin;
     const url = `https://www.amazon.it/dp/${asin}`;
 
@@ -13,17 +12,20 @@ app.get('/api/product/:asin', async (req, res) => {
         const $ = cheerio.load(data);
 
         const title = $('#productTitle').text().trim();
-        const price = $('#corePrice_feature_div span.a-offscreen').first().text().trim();
+        const price = $('#corePrice_feature_div span.a-offscreen').text().trim();
         const image = $('#landingImage').attr('src');
-        const urlProduct = url + "/?&tag=angelblack199-21";
 
-        res.json({ title, price, image, urlProduct });
+        const product = {
+            title,
+            price,
+            image
+        };
+
+        res.json(product);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred' });
     }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server started');
-});
+module.exports = router;
